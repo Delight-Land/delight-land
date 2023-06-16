10931a1e9f2eb4154bf7c93ee50423d49114fc9e
jj0809 <jjyoun48@gmail.com>
2023-06-16 16:39:32 +0900

    spring boot create

---
+++ delight-land/src/main/java/com/delightland/DelightLandApplication.java
@@ -0,0 +1,13 @@
+package com.delightland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelightLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelightLandApplication.class, args);
+	}
+
+}
---
+++ delight-land/src/test/java/com/delightland/delightland/DelightLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delightland.delightland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelightLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
