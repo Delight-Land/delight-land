31c4395be559cca6b48f5aebde027e3daefc1888
hamsuri <yebear96@gmail.com>
2023-08-14 15:14:02 +0900

    Radio DB 연결

--- delight-land/src/main/java/com/delightland/controller/RadioController.java
+++ delight-land/src/main/java/com/delightland/controller/RadioController.java
@@ -1,13 +1,14 @@
 package com.delightland.controller;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delightland.service.RadioService;
-
 @RestController
 @RequestMapping("/radio")
 public class RadioController {
@@ -15,10 +16,14 @@ public class RadioController {
   @Autowired
   private RadioService radioService;
 
-  @PostMapping("/self")
-  public void RadioOn() {
-    String radioPath = radioService.getObject("01. 캐해불가 셒구.mp3");
+  @PostMapping("/self/{radio_no}")
+  public String RadioOn(@PathVariable int radio_no) {
+    
+    String result = radioService.findByno(radio_no);
+    String radioPath = radioService.getObject(result);
     System.out.println(radioPath);
+    
+    return radioPath;
   }
   
 }
---
+++ delight-land/src/main/java/com/delightland/domain/Radio.java
@@ -0,0 +1,16 @@
+package com.delightland.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Radio {
+
+  private int radio_no;
+  private String radio_name;
+}
