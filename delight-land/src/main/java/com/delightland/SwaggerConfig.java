a3e46eab36734f1ee38f18215ce1eb5a944a3492
jjyoun-intu <jjyoun@intuworks.co.kr>
2023-08-16 11:03:13 +0900

    swagger 추가

---
+++ delight-land/src/main/java/com/delightland/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.delightland;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.OAS_30)
+        .apiInfo(apiInfo())
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+        .title("SpringBoot Practice Rest Api Document")
+        .description("springboot rest api practice")
+        .version("0.1")
+        .build();
+    }
+
+}
